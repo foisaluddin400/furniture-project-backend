902cf5dae7ead110c682af1414afa24c1307eda2
foisal <foisalrk2@gmail.com>
2024-10-11 19:00:30 +0600

    updateddd

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
@@ -15,9 +14,9 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 
-const uri = 'mongodb://localhost:27017'
+// const uri = 'mongodb://localhost:27017'
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlk7a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlk7a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("furniture")
     const menuCollection = database.collection("menu")
@@ -278,7 +277,7 @@ async function run() {
       }
     });
 
-    
+
         app.get('/payments/:email', verifyToken, async(req, res) => {
           const query ={email: req.params.email}
           if(req.params.email !== req.decoded.email){
@@ -310,8 +309,8 @@ async function run() {
       });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
